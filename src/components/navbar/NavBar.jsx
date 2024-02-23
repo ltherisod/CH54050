0350b52fe5fc6a31e5d8363b33ad33485dc2491e
ltherisod <lauratherisod@gmail.com>
2024-02-22 21:29:25 -0300

    update navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -3,13 +3,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidget from '../cartWidget/CartWidget';
+import logo from '../../images/logo.png'
 
 function NavBar() {
   console.log('soy Navbar y me renderice')
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">CoderApp</Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={logo} alt='logo' style={{width:'7rem'}}/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
